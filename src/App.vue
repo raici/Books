815e650ede9d8707e032a29b25519b989c87bff5
Ivan Raic <raicivan06@gmail.com>
2022-11-15 12:06:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+        app
+        shrink-on-scroll
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Books</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+          v-for="book in books"
+          :key="book.title"
+          cols="4"
+      >
+        <v-card height="200">
+          {{ book.title }}
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  //import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    data(){
+      return{
+        test: 'Testna varijabla',
+        books: []
+      }
+    },
+    created() {
+      this.getData();
+    },
+    methods: {
+      getData(){
+        let api = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json"
+        this.axios.get(api,  {
+          params: {
+            'api-key':  '663gxgca9HwVo1apZbmu3iYD6Gi6Sh6G'
+          }
+        }).then((response)=>{
+          console.log(response.data)
+          this.books = response.data.results
+        })
+      }
+    }
+    //components: {
+      //HelloWorld,
+    //},
+
+
+  }
+</script>
